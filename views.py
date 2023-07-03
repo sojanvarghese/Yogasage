51f6f3ec17d0f080f6e3b54f6a242f4efc73600d
Sojan Varghese <135998487+sojanvarghese@users.noreply.github.com>
2023-07-03 13:47:53 +0530

    views.py

--- views.py
+++ views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render, redirect
 from yoga.models import Email, UserDetails
-import json
-from django.http import JsonResponse
-
-def pwarrior(request):
-    if request.method == 'POST':
-        image_data = request.POST.get('image_data')
-
-        # Process the image data and make predictions
-
-        # Dummy predictions for demonstration purposes
-        predictions = [
-            {'className': 'Label 1', 'probability': 0.8},
-            {'className': 'Label 2', 'probability': 0.2},
-        ]
-
-        return JsonResponse({'predictions': predictions})
-
-    return JsonResponse({'error': 'Invalid request method'})
-
 
 def home(request):
     if request.method == 'POST':
         email = request.POST['email']
-        request.session['email'] = email  # Store email in session
+        request.session['email'] = email
+
         try:
             Email.objects.get(address=email)
             return redirect('getstarted')
@@ -32,11 +14,13 @@ def home(request):
 
     return render(request, 'home.html')
 
+
 def details(request):
     if request.method == 'POST':
-        email = request.session.get('email')  # Retrieve email from session
+        email = request.session.get('email')
+
         if not email:
-            return redirect('home')  # Redirect to home page if email is not found in session
+            return redirect('home') 
 
         name = request.POST['name']
         age = int(request.POST['age'])
@@ -54,20 +38,21 @@ def details(request):
             email_obj = Email.objects.create(address=email)
 
         email_obj.bmi = bmi
+        email_obj.name = name
         email_obj.save()
 
         return redirect('getstarted')
 
     return render(request, 'details.html')
 
+
 def getstarted(request):
     email = request.session.get('email')
     try:
-            email_obj = Email.objects.get(address=email)
+         email_obj = Email.objects.get(address=email)
     except Email.DoesNotExist:
-            email_obj = Email.objects.create(address=email)
+         email_obj = Email.objects.create(address=email)
     try:
-        
         bmi = email_obj.bmi 
         if bmi < 18.5:
             return redirect('uday1')
@@ -75,41 +60,89 @@ def getstarted(request):
             return redirect('nday1')
         elif 25<= bmi <500:
             return redirect('oday1')
-
     except UserDetails.DoesNotExist:
         return render(request, 'details.html')
 
 
-
 def uday1(request):
     return render(request, 'uday1.html')
-
 def uday2(request):
     return render(request, 'uday2.html')
-
 def nday1(request):
     return render(request, 'nday1.html')
-
 def nday2(request):
     return render(request, 'nday2.html')
-
 def oday1(request):
     return render(request, 'oday1.html')
-
 def oday2(request):
     return render(request, 'oday2.html')
 
+
 def treepose(request):
-    return render(request, 'treepose.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'treepose.html', context)
+
+
 def easy(request):
-    return render(request, 'easy.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'easy.html', context)
+
+
 def diamond(request):
-    return render(request, 'diamond.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'diamond.html', context)
+
+
 def goddess(request):    
-    return render(request, 'goddess.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'goddess.html', context)
+
+
 def triangle(request):
-    return render(request, 'triangle.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'triangle.html', context)
+
+    
 def warrior(request):
-    return render(request, 'warrior.html')
+    email = request.session.get('email')
+    try:
+         email_obj = Email.objects.get(address=email)
+    except Email.DoesNotExist:
+         email_obj = Email.objects.create(address=email)
+    name=email_obj.name
+    context = {'name': name}
+    return render(request, 'warrior.html', context)
+
+
 def End(request):
     return render(request, 'End.html')
