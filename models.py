c05b85b2b5e1bf90cff123fb7fa5e2d53323cc40
Sojan Varghese <135998487+sojanvarghese@users.noreply.github.com>
2023-07-03 13:43:42 +0530

    models.py

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Email(models.Model):
     address = models.EmailField(unique=True)
     bmi=models.FloatField(null=True,blank=True)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.address
